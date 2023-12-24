584b35d3a3e6d782e82a74aff64592bb98157fe2
Zeeshan Mukhtar <Zeshanmukhtar0878@gmail.com>
2023-12-23 22:43:42 -0800

    👽️ added GetMessages API

--- Backend/controllers/messageController.js
+++ Backend/controllers/messageController.js
@@ -41,4 +41,23 @@ async function sendMessage(req, res) {
   }
 }
 
-export { sendMessage };
+async function GetMessages(req, res) {
+  const { otherUserId } = req.params;
+  const userId = req.user._id;
+  try {
+    const conversation = await Conversation.findOne({
+      participants: { $all: [userId, otherUserId] },
+    });
+    if (!conversation) {
+      return res.status(404).json({ error: 'Conversation not found' });
+    }
+    const messages = await Message.find({
+      conversationId: conversation._id,
+    }).sort({ createdAt: 1 });
+    res.status(200).json(messages);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+}
+
+export { sendMessage, GetMessages };
--- Backend/routes/messageRoutes.js
+++ Backend/routes/messageRoutes.js
@@ -1,8 +1,9 @@
 import express from 'express';
 //  adding .js is very important here
 import protectRoute from '../middlewares/protectRoute.js';
-import { sendMessage } from '../controllers/messageController.js';
+import { GetMessages, sendMessage } from '../controllers/messageController.js';
 const router = express.Router();
 router.post('/', protectRoute, sendMessage);
+router.get('/:otherUserId', protectRoute, GetMessages);
 
 export default router;
