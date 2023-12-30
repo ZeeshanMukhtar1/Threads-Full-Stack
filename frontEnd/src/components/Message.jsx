328d8207c608c2e9de9803f30bbfa9386cf3d03b
Zeeshan Mukhtar <Zeshanmukhtar0878@gmail.com>
2023-12-30 10:46:32 -0800

    🧪  Add image support to Message component

--- frontEnd/src/components/Message.jsx
+++ frontEnd/src/components/Message.jsx
@@ -1,4 +1,4 @@
-import { Avatar, Box, Flex, Text } from '@chakra-ui/react';
+import { Avatar, Box, Flex, Image, Text } from '@chakra-ui/react';
 
 import React from 'react';
 import { useRecoilValue } from 'recoil';
@@ -15,20 +15,34 @@ export const Message = ({ OwnMessage, message }) => {
     <>
       {OwnMessage ? (
         <Flex gap={2} alignSelf={'flex-end'}>
-          <Flex bg={'green.800'} maxW={'350px'} p={1} borderRadius={'md'}>
-            <Text color={'white'}>{message.text}</Text>
-            <Box alignSelf={'flex-end'} ml={1} color={message.seen ? 'black.400' : ''} fontWeight={'bold'}>
-              <BsCheck2All />
-            </Box>
-          </Flex>
+          {false && (
+            <Flex bg={'green.800'} maxW={'350px'} p={1} borderRadius={'md'}>
+              <Text color={'white'}>{message.text}</Text>
+              <Box alignSelf={'flex-end'} ml={1} color={message.seen ? 'black.400' : ''} fontWeight={'bold'}>
+                <BsCheck2All />
+              </Box>
+            </Flex>
+          )}
+          {message.img && (
+            <Flex mt={5} w={'200px'}>
+              <Image src="https://avatars.githubusercontent.com/u/91063160?v=4" alt="msg img" borderRadius={4} />
+            </Flex>
+          )}
           <Avatar src={user.ProfilePic || 'https://avatars.githubusercontent.com/u/91063160?v=4'} w={7} h={7} />
         </Flex>
       ) : (
         <Flex gap={2}>
           <Avatar src="" w={7} h={7} />
-          <Text maxW={'350px'} p={1} bg={'gray.400'} color={'black'} borderRadius={'md'}>
-            {message.text}
-          </Text>
+          {true && (
+            <Text maxW={'350px'} p={1} bg={'gray.400'} color={'black'} borderRadius={'md'}>
+              {message.text}
+            </Text>
+          )}
+          {message.img && (
+            <Flex mt={5} w={'200px'}>
+              <Image src="https://avatars.githubusercontent.com/u/91063160?v=4" alt="msg img" borderRadius={4} />
+            </Flex>
+          )}
         </Flex>
       )}
     </>
