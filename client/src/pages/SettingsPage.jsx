6c2158dd0190908d741181d6613030ad6937b8ee
Zeeshan Mukhtar <Zeshanmukhtar0878@gmail.com>
2023-12-31 21:17:45 -0800

    Refactor SettingsPage and Header components

--- client/src/pages/SettingsPage.jsx
+++ client/src/pages/SettingsPage.jsx
@@ -1,42 +1,61 @@
-import { Button, Text } from "@chakra-ui/react";
-import useShowToast from "../hooks/useShowToast";
-import useLogout from "../hooks/useLogout";
+import {
+  Button,
+  Text,
+  VStack,
+  Center,
+  Heading,
+  useColorModeValue,
+} from '@chakra-ui/react';
+import useShowToast from '../hooks/useShowToast';
+import useLogout from '../hooks/useLogout';
 
 export const SettingsPage = () => {
-	const showToast = useShowToast();
-	const logout = useLogout();
+  const showToast = useShowToast();
+  const logout = useLogout();
 
-	const freezeAccount = async () => {
-		if (!window.confirm("Are you sure you want to freeze your account?")) return;
+  const freezeAccount = async () => {
+    if (!window.confirm('Are you sure you want to freeze your account?'))
+      return;
 
-		try {
-			const res = await fetch("/api/users/freeze", {
-				method: "PUT",
-				headers: { "Content-Type": "application/json" },
-			});
-			const data = await res.json();
+    try {
+      const res = await fetch('/api/users/freeze', {
+        method: 'PUT',
+        headers: { 'Content-Type': 'application/json' },
+      });
+      const data = await res.json();
 
-			if (data.error) {
-				return showToast("Error", data.error, "error");
-			}
-			if (data.success) {
-				await logout();
-				showToast("Success", "Your account has been frozen", "success");
-			}
-		} catch (error) {
-			showToast("Error", error.message, "error");
-		}
-	};
+      if (data.error) {
+        return showToast('Error', data.error, 'error');
+      }
+      if (data.success) {
+        await logout();
+        showToast(
+          'Success',
+          'Your account has been temporarily deactivated',
+          'success'
+        );
+      }
+    } catch (error) {
+      showToast('Error', error.message, 'error');
+    }
+  };
 
-	return (
-		<>
-			<Text my={1} fontWeight={"bold"}>
-				Freeze Your Account
-			</Text>
-			<Text my={1}>You can unfreeze your account anytime by logging in.</Text>
-			<Button size={"sm"} colorScheme='red' onClick={freezeAccount}>
-				Freeze
-			</Button>
-		</>
-	);
+  return (
+    <VStack spacing={4} align="center" justify="center">
+      <Heading as="h2" size="lg" fontWeight="bold">
+        Freeze Your Account
+      </Heading>
+      <Text
+        fontSize="sm"
+        textAlign="center"
+        color={useColorModeValue('gray.600', 'gray.400')}
+      >
+        Temporarily deactivate your account. You can unfreeze it anytime by
+        logging in. your data will be preserved!
+      </Text>
+      <Button colorScheme="red" onClick={freezeAccount}>
+        Freeze Account
+      </Button>
+    </VStack>
+  );
 };
