befd69f151a1816ef77ce284f21f506b1b9c4208
Zeeshan Mukhtar <zeshanmukhtar878@gmail.com>
2024-04-21 21:52:40 +0500

    👽️ added delete chat functionality

--- server/routes/messageRoutes.js
+++ server/routes/messageRoutes.js
@@ -1,11 +1,17 @@
-import express from "express";
-import protectRoute from "../middlewares/protectRoute.js";
-import { getMessages, sendMessage, getConversations } from "../controllers/messageController.js";
+import express from 'express';
+import protectRoute from '../middlewares/protectRoute.js';
+import {
+  getMessages,
+  sendMessage,
+  getConversations,
+  deleteChat,
+} from '../controllers/messageController.js';
 
 const router = express.Router();
 
-router.get("/conversations", protectRoute, getConversations);
-router.get("/:otherUserId", protectRoute, getMessages);
-router.post("/", protectRoute, sendMessage);
+router.get('/conversations', protectRoute, getConversations);
+router.get('/:otherUserId', protectRoute, getMessages);
+router.post('/', protectRoute, sendMessage);
+router.delete('/chat/:conversationId', protectRoute, deleteChat);
 
 export default router;
